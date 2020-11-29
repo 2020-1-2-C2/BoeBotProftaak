bb7e93adecb4771c0041e155a7ce6bcaf8a4feb2
Lars Hoendervangers <unconfigured@null.spigotmc.org>
2020-11-29 15:13:07 +0100

    -add: Buzzer class -add: TestMain class

---
+++ src/Hardware/Buzzer.java
@@ -0,0 +1,61 @@
+package Hardware;
+
+import TI.BoeBot;
+
+public class Buzzer {
+    private int pinId;
+    private boolean isOn;
+
+    /**
+     *
+     * @param pinId is the pin where the buzzer is connected to.
+     */
+    public Buzzer(int pinId) {
+        this.pinId = pinId;
+    }
+
+    /**
+     * Buzz for 1 second on 1000Hz.
+     */
+    public void buzz() {
+        buzz(1000,1000);
+    }
+
+    /**
+     * Buzz for a certain amount of time on 1000Hz.
+     * @param time in milliseconds.
+     */
+    public void buzz(int time) {
+        buzz(time,1000);
+    }
+
+    /**
+     * Buzz for a certain amount of time on a certain frequency.
+     * @param time in milliseconds.
+     * @param freq in Hz.
+     */
+    public void buzz(int time, int freq) {
+        BoeBot.freqOut(this.pinId, freq, time);
+    }
+
+    /**
+     * Buzz for half a second.
+     */
+    public void beep() {
+        buzz(500);
+    }
+
+    /**
+     * Turns the buzzer off. (If the buzzer won't shut off automatically.)
+     */
+    public void off() {
+        this.isOn = false;
+    }
+
+    /**
+     * @return the isOn boolean.
+     */
+    public boolean getIsOn() {
+        return this.isOn;
+    }
+}
--- src/RobotMain.java
+++ src/RobotMain.java
@@ -1,3 +1,4 @@
+import Hardware.Buzzer;
 import Hardware.Led;
 import Hardware.Motors;
 import Hardware.RGB;
@@ -7,40 +8,6 @@ import Utils.Updatable;
 import java.util.ArrayList;
 
 public class RobotMain {
-
-    private ArrayList<Updatable> updatables = new ArrayList<>();
-
-    public void Run() {
-        Led led = new Led(11);
-        led.blink(1000);
-        updatables.add(led);
-
-        int i = 0;
-
-        while (true) {
-            if (i > 1000) {
-                led.off();
-                System.out.println("Hardware.Led off");
-            } else {
-                for (Updatable u : updatables) {
-                    u.Update();
-                }
-            }
-            i++;
-            BoeBot.wait(10);
-        }
-    }
-
     public static void main(String[] args) {
-        RobotMain main = new RobotMain();
-        main.Run();
     }
-
-/*    public static void main(String[] args) {
-        Hardware.Motors motor = new Hardware.Motors();
-        motor.drive(1700);
-        BoeBot.wait(1000);
-        motor.goToSpeed(200,10);
-        motor.emergencyBrake();
-    }*/
 }
