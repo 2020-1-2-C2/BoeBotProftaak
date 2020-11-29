631c1f1c2eea4eb9b2bb55c2ae71233aba539b7d
Lars Hoendervangers <unconfigured@null.spigotmc.org>
2020-11-29 14:11:06 +0100

    -add: Led class -add: Updatable interface

---
+++ src/Led.java
@@ -0,0 +1,68 @@
+import TI.BoeBot;
+import TI.PWM;
+import TI.Timer;
+
+public class Led implements Updatable {
+    private int pinId;
+    private boolean isOn;
+    private Timer blinkingTimer;
+    private int interval;
+
+    /**
+     * Constructor for Led class
+     * @param pinId the pin where the LED is attached.
+     */
+    public Led(int pinId) {
+        this.pinId = pinId;
+    }
+
+    public void on() {
+        this.isOn = true;
+        BoeBot.digitalWrite(this.pinId, false);
+    }
+
+    public void off() {
+        this.isOn = false;
+        BoeBot.digitalWrite(this.pinId, true);
+    }
+
+    /**
+     * Fade the LED
+     * @param fade fades the LED. (Value: 0-255, 0 is full brightness, 255 is off.
+     */
+    public void fade(int fade) {
+        PWM pwm = new PWM(this.pinId, fade);
+        pwm.update(fade);
+    }
+
+    /**
+     * Blink the LED
+     * @param interval is the interval between each blink.
+     */
+    public void blink(int interval) {
+        this.interval = interval;
+        if (interval > 0) {
+            this.blinkingTimer = new Timer(interval);
+        }
+        if (getIsOn()) {
+            off();
+        } else {
+            on();
+        }
+    }
+
+    public boolean getIsOn() {
+        return isOn;
+    }
+
+    /**
+     * Updates the blinker
+     */
+    @Override
+    public void Update() {
+        if (this.blinkingTimer.timeout()) {
+            blink(this.interval);
+            this.blinkingTimer.mark();
+        }
+    }
+}
