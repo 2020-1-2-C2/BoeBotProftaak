c076da6afaba8af21da96c6d682f9b4e5e35a856
Casper Lous <71557728+Chunky1306@users.noreply.github.com>
2020-12-01 15:23:08 +0100

    improve notifications

--- src/Logic/Notifications.java
+++ src/Logic/Notifications.java
@@ -12,33 +12,63 @@ public class Notifications implements Updatable {
 
     private ArrayList<Buzzer> buzzers;
     private ArrayList<Led> leds;
-    private Timer notificationTimer;
+    private Timer buzzerTimer;
+    private Timer ledTimer;
+    private int buzzerTime;
+    private int buzzerFrequency;
+    private int ledInterval;
+    private int buzzerInterval;
+    private boolean timerIsEnabled = false;
+    private boolean repeat = false;
 
     public Notifications(ArrayList<Buzzer> buzzers, ArrayList<Led> leds) {
         this.buzzers = buzzers;
         this.leds = leds;
-        notificationTimer = new Timer(1000);
+        buzzerTimer = new Timer(1000);
+        ledTimer = new Timer(1000);
+
     }
 
     public void emergencyNotification() {
+        this.buzzerFrequency = 400;
+        this.buzzerTime = 500;
+        this.ledInterval = 500;
+        this.buzzerInterval = 1000;
+        this.timerIsEnabled = true;
+
+
+        buzzerTimer.setInterval(this.buzzerInterval);
+
+    }
+
+    public void remoteNotification(){
+        this.timerIsEnabled = false;
+
         for (Led led : leds) {
             if (led instanceof RGB) {
                 ((RGB) led).setColor(true, false, false);
             }
-            //TODO maybe put everything in one timer.
-            led.blink(500);
+
+            led.blink(ledInterval);
         }
-
-        notificationTimer.setInterval(1000);
-
     }
 
     @Override
     public void update() {
-        if (notificationTimer.timeout()) {
+        if (buzzerTimer.timeout() && timerIsEnabled) {
             for (Buzzer buzzer : buzzers) {
-                buzzer.buzz(500, 400);
+                buzzer.buzz(buzzerTime, buzzerFrequency);
             }
+        }
+        if (ledTimer.timeout() && timerIsEnabled)
+        for (Led led : leds) {
+            if (led instanceof RGB) {
+                ((RGB) led).setColor(true, false, false);
+            }
+            else{
+                led.on();
+            }
+
         }
     }
 }
