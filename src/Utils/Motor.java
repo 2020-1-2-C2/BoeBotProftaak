394fdac370eb72bd409c00089d49144d32860bab
Meindert Kempe <mail@mkempe.com>
2020-12-02 15:17:06 +0100

    Document Motor interface

--- src/Utils/Motor.java
+++ src/Utils/Motor.java
@@ -1,10 +1,42 @@
 package Utils;
 
 public interface Motor extends Updatable{
+
+    /**
+     * Got to a certain speed over a certain time
+     * @param speed percentage of the maximum speed (100 max forwards, -100 max backwards)
+     * @param time time in milliseconds over which to accelerate
+     */
     void goToSpeed(int speed, int time);
+
+    /**
+     * Got to a certain speed over a certain time
+     * @param speed percentage of the maximum speed (100 max forwards, -100 max backwards)
+     * @param time time in milliseconds over which to accelerate
+     */
     void goToSpeedLeft(int speed, int time);
+
+    /**
+     * Got to a certain speed over a certain time
+     * @param speed percentage of the maximum speed (100 max forwards, -100 max backwards)
+     * @param time time in milliseconds over which to accelerate
+     */
     void goToSpeedRight(int speed, int time);
+
+    /**
+     * Stop as soon as possible
+     */
     void emergencyStop();
-    void getSpeedLeft();
-    void getSpeedRight();
+
+    /**
+     * Get the current speed in a percentage of the maximum speed
+     * @return percentage of the maximum speed (100 max forwards, -100 max backwards)
+     */
+    int getSpeedLeft();
+
+    /**
+     * Get the current speed in a percentage of the maximum speed
+     * @return percentage of the maximum speed (100 max forwards, -100 max backwards)
+     */
+    int getSpeedRight();
 }
