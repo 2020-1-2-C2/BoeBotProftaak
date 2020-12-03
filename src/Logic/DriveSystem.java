c119a3779bb20a63ca73ca6c29c0c43dbfaed1d4
Meindert Kempe <mail@mkempe.com>
2020-12-03 14:35:20 +0100

    Implement turning in drive system

--- src/Logic/DriveSystem.java
+++ src/Logic/DriveSystem.java
@@ -9,26 +9,49 @@ public class DriveSystem implements Updatable {
     private final int MAX_SPEED = 100;
     // time in ms
     private final int ACCELERATION_TIME = 500;
-    private int currentSpeed;
+    private int currentSpeed = 0;
+    private int currentSpeedRight = 0;
+    private int currentSpeedLeft = 0;
 
     public DriveSystem(Motor motors) {
         this.motor = motors;
     }
 
     public void addForwardSpeed(){
-        int newSpeed = ((motor.getSpeedLeft() + motor.getSpeedRight()) / 2) + MAX_SPEED/STEPS;
-        System.out.println(motor.getSpeedLeft());
-        System.out.println(motor.getSpeedRight());
-        System.out.println(MAX_SPEED/STEPS);
-        motor.goToSpeed(newSpeed, ACCELERATION_TIME);
+        addSpeed(true);
     }
 
     public void addBackwardSpeed(){
-        int newSpeed = ((motor.getSpeedLeft() + motor.getSpeedRight()) / 2) - MAX_SPEED/STEPS;
+        addSpeed(false);
+    }
+
+    /**
+     *
+     * @param direction true = forwards, false = backwards
+     */
+    public void addSpeed(boolean direction) {
+        int speedDiff;
+        if (direction) {
+            speedDiff = MAX_SPEED/STEPS;
+        } else {
+            speedDiff = - MAX_SPEED/STEPS;
+        }
+        currentSpeed = currentSpeed + speedDiff;
+
+        if (currentSpeed > 100) {
+            currentSpeed = 100;
+        } else if (currentSpeed < -100) {
+            currentSpeed = -100;
+        }
+
+        currentSpeedRight = currentSpeed;
+        currentSpeedLeft = currentSpeed;
+
         System.out.println(motor.getSpeedLeft());
         System.out.println(motor.getSpeedRight());
         System.out.println(MAX_SPEED/STEPS);
-        motor.goToSpeed(newSpeed, ACCELERATION_TIME);
+        motor.goToSpeed(currentSpeed, ACCELERATION_TIME);
+
     }
 
     public void turnLeft() {
@@ -44,6 +67,38 @@ public class DriveSystem implements Updatable {
      * @param direction true = right, false = left
      */
     private void turn(boolean direction) {
+        int diff = MAX_SPEED/STEPS;
+        int newSpeedRight;
+        int newSpeedLeft;
+
+        if (direction) {
+            currentSpeedRight = currentSpeedRight - diff;
+            currentSpeedLeft = currentSpeedLeft + diff;
+            if (currentSpeedLeft > 100) {
+                currentSpeedLeft = 100;
+                currentSpeedRight = 80;
+            } else if (currentSpeedLeft < -100) {
+                currentSpeedLeft = -100;
+                currentSpeedRight = -80;
+            }
+        } else {
+            currentSpeedRight = currentSpeedRight + diff;
+            currentSpeedLeft = currentSpeedLeft - diff;
+            if (currentSpeedRight > 100) {
+                currentSpeedRight = 100;
+                currentSpeedLeft = 80;
+            } else if (currentSpeedRight < -100) {
+                currentSpeedRight = -100;
+                currentSpeedLeft = -80;
+            }
+        }
+        currentSpeed = (currentSpeedLeft + currentSpeedRight) / 2;
+
+        motor.goToSpeedRight(currentSpeedRight, ACCELERATION_TIME);
+        motor.goToSpeedLeft(currentSpeedLeft, ACCELERATION_TIME);
+
+
+
         /*
         stop();
         int speedStep = (MAX_FORWARD_SPEED - STOP_SPEED) / STEPS;
